b3971a659de88ea1d36951eecf53aa038341e31f
Nourine Mohammed Nadir <nounadir@gmail.com>
2024-09-21 13:42:45 +0100

    Actor Critic algorithm with PyTorch

---
+++ RL/Gymnasium/Mini projects/Deep Q-Learning/Torch/Actor_Critic/Lunar Lander/Actor_Critic.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+
+class ActorCritic(nn.Module):
+    def __init__(self):
+        super(ActorCritic, self).__init__()
+        self.affine = nn.Linear(8, 128)
+
+        self.action_layer = nn.Linear(128, 4)
+        self.value_layer = nn.Linear(128, 1)
+
+        self.logprobs = []
+        self.state_values = []
+        self.rewards = []
+
+    def forward(self, state):
+        state = torch.from_numpy(state).float()
+        state = F.relu(self.affine(state))
+
+        state_value = self.value_layer(state)
+
+        action_probs = F.softmax(self.action_layer(state))
+        action_distribution = Categorical(action_probs)
+        action = action_distribution.sample()
+
+        self.logprobs.append(action_distribution.log_prob(action))
+        self.state_values.append(state_value)
+
+        return action.item()
+
+    def calculateLoss(self, gamma=0.99):
+
+        # calculating discounted rewards:
+        rewards = []
+        dis_reward = 0
+        for reward in self.rewards[::-1]:
+            dis_reward = reward + gamma * dis_reward
+            rewards.insert(0, dis_reward)
+
+        # normalizing the rewards:
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std())
+
+        loss = 0
+        for logprob, value, reward in zip(self.logprobs, self.state_values, rewards):
+            advantage = reward - value.item()
+            action_loss = -logprob * advantage
+            value_loss = F.smooth_l1_loss(value, reward)
+            loss += (action_loss + value_loss)
+        return loss
+
+    def clearMemory(self):
+        del self.logprobs[:]
+        del self.state_values[:]
+        del self.rewards[:]
---
+++ RL/Gymnasium/Mini projects/Deep Q-Learning/Torch/Actor_Critic/Lunar Lander/Agent.py
@@ -0,0 +1,83 @@
+import json
+# from Actor import Actor_Network
+# from Critic import Critic_Network
+from Actor_Critic import ActorCritic
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+import numpy as np
+
+import os, pickle
+
+
+class Agent():
+    def __init__(self,
+                 batch_size: float,
+                 input_shape,
+                 layer1_nodes,
+                 layer2_nodes,
+                 layer3_nodes,
+                 n_actions: int,
+                 max_mem_size: int = 10_000,
+                 gamma: float = 0.99,
+                 initial_eps: float = 1.0,
+                 final_eps: float = 0.01,
+                 eps_decay: float = 1e-4,
+                 lr: float = 1e-4,
+                 update_freq : int =100
+                 ):
+        self.gamma = gamma
+        self.batch_size = batch_size
+        self.input_shape = input_shape
+        self.eps_decay = eps_decay
+        self.lr = lr
+        self.eps = initial_eps
+        self.eps_decay = eps_decay
+        self.final_eps = final_eps
+        self.mem_counter = 0
+        self.sequence_length = 32  # You can adjust this value
+
+        self.target_update_frequency = update_freq
+        self.learn_step_counter = 0
+        self.ActorCritic = ActorCritic()
+        self.optimizer = optim.Adam(self.ActorCritic.parameters(), lr=self.lr, betas=(0.9, 0.999))
+
+
+
+
+
+
+    def learn(self):
+
+        self.optimizer.zero_grad()
+        loss = self.ActorCritic.calculateLoss()
+        loss.backward()
+        self.optimizer.step()
+        self.ActorCritic.clearMemory()
+
+
+        self.learn_step_counter += 1
+        self.eps = max((self.eps - self.eps_decay), self.final_eps)
+
+
+
+    def save_model(self, PATH):
+        T.save(self.Actor.state_dict(), PATH)
+
+    def load_model(self, q_eval_path ):
+        try:
+            self.Actor = T.load(q_eval_path, map_location=self.device)
+        except FileNotFoundError:
+            print(f"Error: Could not find model files at {q_eval_path }")
+        except RuntimeError as e:
+            print(f"Error loading model: {e}")
+
+
+    def save_agent(self, path):
+        directory = os.path.dirname(path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(path, 'wb') as f:
+            pickle.dump(self, f)
