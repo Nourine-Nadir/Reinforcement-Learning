0b1ca6d1c80f5fbe6dbca670782b22f3c4e2e024
Nourine-Nadir <nounadir@gmail.com>
2025-04-07 20:15:39 +0100

    Update continuous PPO

--- RL/Gymnasium/Mini projects/PPO/Continuous_action_space/MountainCar/main.py
+++ RL/Gymnasium/Mini projects/PPO/Continuous_action_space/MountainCar/main.py
@@ -3,7 +3,7 @@ from Train import train
 from Test import test
 
 if __name__ == '__main__':
-    env = gym.make('MountainCarContinuous-v0', render_mode=None, max_episode_steps=8000)
+    env = gym.make('MountainCarContinuous-v0', render_mode='human', max_episode_steps=8000)
     env.metadata['render_fps'] = 60  # Adjust FPS
 
     train(save_model=True, plot_name='Mountain PPO ', environement=env)
